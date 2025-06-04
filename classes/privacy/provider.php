e4d179e66e8f8425871557aef34edaa34f723824
Andrew Madden <andrew.madden@didasko-online.com>
2025-06-04 14:49:31 +1000

    feat: Create plugin skeleton

---
+++ classes/privacy/provider.php
@@ -0,0 +1,40 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Define the personal data stored by plugin.
+ *
+ * @package    availability_capability
+ * @author     Andrew Madden <andrewmadden@didasko-online.com>
+ * @copyright  2025 Didasko Online
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace availability_capability\privacy;
+
+use core_privacy\local\metadata\null_provider;
+
+class provider implements null_provider {
+
+    /**
+     * Get a human-readable string why this plugin does not provide personal data.
+     *
+     * @return string
+     */
+    public static function get_reason(): string {
+        return 'privacy:metadata';
+    }
+}
---
+++ lang/en/availability_capability.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language strings.
+ *
+ * @package    availability_capability
+ * @author     Andrew Madden <andrewmadden@didasko-online.com>
+ * @copyright  2025 Didasko Online
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['description'] = 'Restrict access based on user capabilities';
+$string['pluginname'] = 'Restriction by capability';
+$string['privacy:metadata'] = 'The Restriction by capability plugin does not store any personal data.';
+$string['title'] = 'Capability';
